02fe7e837ade5b1460789a0ffff57cb28b6e1d8e
Gyanendra Mohan Patel <gmpatel152@gmail.com>
2026-06-14 11:41:02 +0530

    dynamic path

--- src/json_read.py
+++ src/json_read.py
@@ -3,5 +3,13 @@ import json
 with open("orders.json" , "r") as f:
     data = json.load(f)
     
+    
+rows = []
 for order in data["orders"]:
-    print(order)
+    for item in order["items"]:
+        rows.append({
+            "order_id":order["id"],
+            "sku":item["sku"],
+            "qty":item["qty"]
+        })
+print(rows)
---
+++ src/os_demo.py
@@ -0,0 +1,8 @@
+# dynamic path 
+
+from pathlib import Path
+
+base_path = Path(__file__).resolve().parent.parent
+data_path = base_path/"data"/"raw"
+
+print(data_path)
