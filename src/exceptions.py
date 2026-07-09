c5469d497036f135b2daeaf01390cba7c24bef36
Gyanendra Mohan Patel <gmpatel152@gmail.com>
2026-07-09 15:44:27 +0530

    TypeError

--- src/exceptions.py
+++ src/exceptions.py
@@ -6,5 +6,17 @@
 # finally:
 #     print("cleanup complete")
 
-with open("missing_file.csv", "r") as f:
-    data = f.read()
+# with open("missing_file.csv", "r") as f:
+#     data = f.read()
+
+try:
+    amout = "100"
+    total = amout + 50
+except TypeError as e:
+    print(f"Error: {e}")
+    amount = int("100")
+    total = amount + 50
+    print("\nAfter conversion\n")
+    print(f"Total: {total}\n")
+finally:
+    print("cleanup complete\n")
